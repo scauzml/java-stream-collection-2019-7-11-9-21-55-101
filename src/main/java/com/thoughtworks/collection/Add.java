e0b80e0b0e5fb0122f0d25383f1346d608b6aae7
scauzml <13828607279@163.com>
2019-07-13 23:36:55 +0800

    求数组中所有偶数的平均数

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -52,7 +52,7 @@ public class Add {
 //    }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(e->e%2==0).mapToInt(e->e).average().getAsDouble();
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
